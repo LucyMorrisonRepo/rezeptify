11798d7b5a1ce719c25fb04df3627ce5e1452fd4
kofiMorrison <49091928+kofimorrison@users.noreply.github.com>
2019-07-21 02:41:57 +0200

    Delete config.js

--- src/js/config.js
+++
@@ -1,6 +0,0 @@
-// export const key = "fdfe5eae98e53f07f92cc7250c3b4c96";
-// export const key = "dcb82e7fae690385a596fd35091a90f5";
-// export const key = "cfd8ce54e8b06f791266587964bcf0b1";
-
-// export const key = "743ff3fa13e2b44c0721041bbf13a440";
-export const key = "2866980e378177d850f6bef271993f16";
